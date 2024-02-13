eaa6c6befec018c66b707304a818c652c3db40fa
Pawel Fraczek <pawelfraczek@o2.pl>
2024-02-13 20:45:10 +0100

    add invoke tasks

---
+++ tasks.py
@@ -0,0 +1,19 @@
+from invoke import task
+
+
+@task
+def clean(c, docs=False, bytecode=False, extra=""):
+    patterns = ["build"]
+    if docs:
+        patterns.append("docs/_build")
+    if bytecode:
+        patterns.append("**/*.pyc")
+    if extra:
+        patterns.append(extra)
+    for pattern in patterns:
+        c.run("rm -rf {}".format(pattern))
+
+
+@task
+def clear(c):
+    c.run("rm -rf .venv")
